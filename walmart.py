60646bba47a8ca71f491cdcd17b60d9dbe4dcd09
Milan sharma <milan.sharma@mediaamp.co.in>
2025-02-12 12:19:09 +0530

    Update and rename t.py to walmart.py

--- walmart.py
+++ walmart.py
@@ -1,7 +1,8 @@
 import scrapy
 import json
 from urllib.parse import urlencode
-from ..items import WalmartItems
+from errors.items import WalmartItems
+from logs.error_handler import ErrorManager 
 
 
 API_KEY = ''
@@ -13,17 +14,21 @@ def get_scraperapi_url(url):
 class WalmartSpider(scrapy.Spider):
     name = "walmart"
     start_urls = [
-        "https://www.walmart.com/browse/electronics/all-apple-ipad/3944_1229722_1229728_3998838"
+        "https://www.walmart.com/browse/electronics/all-apple-ipad/3944_1229722_1229728_3998838/?page={page}"
     ]
-
+    def __init__(self, *args, **kwargs):
+        super(WalmartSpider, self).__init__(*args, **kwargs)
+        self.error_manager = ErrorManager()
     def start_requests(self):
         for url in self.start_urls:
-            proxy_url = get_scraperapi_url(url)  # Get proxy URL
-            yield scrapy.Request(
-                proxy_url,
-                # headers=self.settings.get("DEFAULT_REQUEST_HEADERS"),
-                callback=self.parse
-            )
+            for i in range(1, 6):
+                pageurl = url.format(page=i)
+                proxy_url = get_scraperapi_url(pageurl)  # Get proxy URL
+                yield scrapy.Request(
+                    proxy_url,
+                    callback=self.parse,                
+                    # errback=lambda failure: self.error_manager.handle_request_failure(failure, self.name)
+                )
 
     def parse(self, response):
         script_data = response.xpath('//script[@id="__NEXT_DATA__"]/text()').get()
@@ -33,8 +38,10 @@ class WalmartSpider(scrapy.Spider):
             json_data = json.loads(script_data)
             search_data = json_data.get('props', {}).get('pageProps', {}).get('initialData', {}).get('searchResult', {}).get('itemStacks', [])
             items = search_data[0]["items"]
+            # print(items)
             for data in items:
                 item = WalmartItems()
+                # print(data)
                 item['name'] =  data.get("name")
                 item['imageurl'] = data.get("image")
                 item['linePrice'] = data.get("priceInfo", {}).get("linePrice")
@@ -43,5 +50,4 @@ class WalmartSpider(scrapy.Spider):
                 item['sponsored'] = data.get("isSponsoredFlag", False) or (data.get("sponsoredProduct") is not None)
                 item['variants'] = bool(data.get("variantList", []))
                 item['ratings'] = data.get("rating", {})
-                
                 yield item
