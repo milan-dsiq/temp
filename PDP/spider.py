9e4f82c7d1b2a5d77b9a64764295ff771272fe7e
Milan sharma <milan.sharma@mediaamp.co.in>
2025-02-13 20:15:40 +0530

    Update spider.py

--- PDP/spider.py
+++ PDP/spider.py
@@ -14,19 +14,19 @@ def get_scraperapi_url(url):
 class WalmartPDPSpider(scrapy.Spider):
     name = "walmartPDP"
     start_urls = [
-      "https://www.walmart.com/ip/Open-Box-Apple-iPad-Pro-10-5-inch-Retina-256GB-Wi-Fi-Only-Newest-OS-Bundle-Case-Pre-Installed-Tempered-Glass-Rapid-Charger-Bluetooth-Wireless-Airbuds/2992862548?classType=VARIANT&athbdg=L1600"
+        "https://www.walmart.com/ip/Annual-Event-Standard-Bundle/12941505136?classType=REGULAR&athbdg=L1600"
     ]
     def __init__(self, *args, **kwargs):
         super(WalmartPDPSpider, self).__init__(*args, **kwargs)
         self.error_manager = ErrorManager()
     def start_requests(self):
         for url in self.start_urls:
-          proxy_url = get_scraperapi_url(url)  # Get proxy URL
-          yield scrapy.Request(
-              proxy_url,
-              callback=self.parse,                
-              # errback=lambda failure: self.error_manager.handle_request_failure(failure, self.name)
-          )
+            proxy_url = get_scraperapi_url(url)  # Get proxy URL
+            yield scrapy.Request(
+                proxy_url,
+                callback=self.parse,                
+                errback=lambda failure: self.error_manager.handle_request_failure(failure, self.name)
+            )
 
     def parse(self, response):
         script_data = response.xpath('//script[@id="__NEXT_DATA__"]/text()').get()
@@ -37,35 +37,37 @@ class WalmartPDPSpider(scrapy.Spider):
             test = []
             my_data = json_data.get('props', {}).get('pageProps', {}).get('initialData', {}).get('data', {})
             
-            test.append({
-              "primaryProductId": my_data["products"].get("primaryProductId"),
-              "abstractProductId": my_data["products"].get("abstractProductId"),
-              "primaryUsItemId": my_data["products"].get("primaryUsItemId"),
-              "title": my_data["products"].get("name"),
-              "short_description": my_data["idml"].get("shortDescription"),
-              "long_description": my_data["idml"].get("longDescription"),
-              "category": my_data["products"].get("category"),
-              "brand": my_data["products"].get("brand"),
-              "brandUrl": my_data["products"].get("brandUrl"),
-              "warranty": my_data["idml"].get("warranty"),
-              "warnings": my_data["idml"].get("warnings"),
-              "nutritionFacts": my_data["idml"].get("nutritionFacts"),
-              "warranty": my_data["idml"].get("warranty"),
-              "badges": my_data["products"].get("badges"),
-              "currentPrice": my_data["products"].get("priceInfo").get("currentPrice").get("priceString"),
-              "wasPrice": my_data["products"].get("wasPrice").get("priceString"),
-              "sellerName": my_data["products"].get("sellerName"),
-              "sellerURL": my_data["products"].get("sellerStoreFrontURL"),
-              "images": my_data["products"].get("imageInfo"),
-              "variantCriteria": my_data["products"].get("variantCriteria", []),
-              "productDetailPageUrl": my_data["products"].get("canonicalUrl"),
-              "subscriptionPrice": my_data["products"].get("subscriptionPrice"),
-              "breadCrumbs": my_data["seoItemMetaData"].get("breadCrumbs", []),
-              "totalReviewCount": my_data["reviews"].get("totalReviewCount"),
-              "averageOverallRating": my_data["reviews"].get("averageOverallRating"),
-              "sponserdAd": my_data["contentLayout"].get("modules")[4]
+            try:
+                test.append({
+                "primaryProductId": my_data.get("product", {}).get("primaryProductId", {}),
+                "abstractProductId": my_data.get("product", {}).get("abstractProductId", {}),
+                "primaryUsItemId": my_data.get("product", {}).get("primaryUsItemId", {}),
+                "productTypeId": my_data.get("product", {}).get("productTypeId",{}),
+                "title": my_data.get("product", {}).get("name", {}),
+                "short_description": my_data.get("idml", {}).get("shortDescription", {}),
+                "long_description": my_data.get("idml", {}).get("longDescription", {}),
+                "category": my_data.get("product", {}).get("category", {}),
+                "brand": my_data.get("product", {}).get("brand", {}),
+                "brandUrl": my_data.get("product", {}).get("brandUrl", {}),
+                "warranty": my_data.get("idml", {}).get("warranty", {}),
+                "warnings": my_data.get("idml", {}).get("warnings", {}),
+                "nutritionFacts": my_data.get("idml", {}).get("nutritionFacts", {}),
+                "badges": my_data.get("product", {}).get("badges", {}),
+                "priceInfo": my_data.get("product", {}).get("priceInfo", {}),
+                "sellerName": my_data.get("product", {}).get("sellerName", {}),
+                "sellerURL": my_data.get("product", {}).get("sellerStoreFrontURL", {}),
+                "images": my_data.get("product", {}).get("imageInfo", {}),
+                "interactiveProductVideo": my_data.get("idml", {}).get("interactiveProductVideo",{}),
+                "variantCriteria": my_data.get("product", {}).get("variantCriteria", {}),
+                "productDetailPageUrl": my_data.get("product", {}).get("canonicalUrl", {}),
+                "subscriptionPrice": my_data.get("product", {}).get("subscriptionPrice"),
+                "breadCrumbs": my_data.get("seoItemMetaData", {}).get("breadCrumbs", {}),
+                "totalReviewCount": my_data.get("reviews", {}).get("totalReviewCount", {}),
+                "averageOverallRating": my_data.get("reviews", {}).get("averageOverallRating", {}),
+                # "sponserdAd": my_data.get("contentLayout", {}).get("modules", {})[4]
             })
-            print(test)
+            except Exception as e:
+                self.error_manager.log_parsing_error(response, {str(e)}, self.name)
             with open("final_data.json", "a", encoding="utf-8") as f:
                 json.dump(test[0], f, indent=4, ensure_ascii=False)
                 f.write("\n")  # New line for each page’s data
